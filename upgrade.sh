391da8ee5d7752de7064f586270394eaedfe45d5
robert.priedl@priedl-consulting.com <robert.priedl@priedl-consulting.com>
2023-07-03 10:11:25 +0200

    add upgrade script for awx

---
+++ upgrade.sh
@@ -0,0 +1,34 @@
+# upgrade awx instance on k3s
+# from https://computingforgeeks.com/how-to-upgrade-ansible-awx-running-in-kubernetes/?expand_article=1
+
+# change to home dir and remove old repo
+cd ~
+rm awx-operator -r -f
+# clone actual awx-operator
+git clone https://github.com/ansible/awx-operator.git
+cd awx-operator
+
+# set namespace to awx
+kubectl config set-context --current --namespace=awx
+
+# install curl and jq
+sudo yum install epel-release
+sudo yum install curl jq
+
+# delete old awx container
+kubectl delete  deployment awx-operator-controller-manager
+kubectl  delete serviceaccount awx-operator-controller-manager
+kubectl delete rolebinding awx-operator-awx-manager-rolebinding
+kubectl delete role awx-operator-awx-manager-role
+
+# deploy new version of awx-operator
+export NAMESPACE=awx
+VERSION=latest make deploy
+
+# operator pod should be pulled and installed
+kubectl get pods
+
+# awx and postgres pods should be provisioned in new version
+# installation logs can be viewd in:
+kubectl logs -f deployments/awx-operator-controller-manager -c awx-manager
+
