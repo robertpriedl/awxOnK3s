5f33776f726b12ba71afded17b60ea02d632c05f
robert.priedl@priedl-consulting.com <robert.priedl@priedl-consulting.com>
2023-09-22 08:59:10 +0200

    add full path to kubectl for sudo

--- upgrade.sh
+++ upgrade.sh
@@ -8,27 +8,27 @@ git clone https://github.com/ansible/awx-operator.git ~/awx-operator
 #source ~/awx-operator
 
 # set namespace to awx
-kubectl config set-context --current --namespace=awx
+/usr/local/bin/kubectl config set-context --current --namespace=awx
 
 # install curl and jq
 sudo yum install epel-release -y
 sudo yum install curl jq -y
 
 # delete old awx container
-kubectl delete  deployment awx-operator-controller-manager -n awx
-kubectl  delete serviceaccount awx-operator-controller-manager -n awx
-kubectl delete rolebinding awx-operator-awx-manager-rolebinding -n awx
-kubectl delete role awx-operator-awx-manager-role -n awx
+/usr/local/bin/kubectl delete deployment awx-operator-controller-manager -n awx
+/usr/local/bin/kubectl delete serviceaccount awx-operator-controller-manager -n awx
+/usr/local/bin/kubectl delete rolebinding awx-operator-awx-manager-rolebinding -n awx
+/usr/local/bin/kubectl delete role awx-operator-awx-manager-role -n awx
 
 # deploy new version of awx-operator
 export NAMESPACE=awx
 (cd ~/awx-operator && VERSION=latest make deploy)
 
 # operator pod should be pulled and installed
-kubectl get pods -n awx
+/usr/local/bin/kubectl get pods -n awx
 
 # awx and postgres pods should be provisioned in new version
 # installation logs can be viewd in:
 echo "to get logs call:"
-echo "kubectl logs -f deployments/awx-operator-controller-manager -c awx-manager -n awx"
+echo "/usr/local/bin/kubectl logs -f deployments/awx-operator-controller-manager -c awx-manager -n awx"
 
